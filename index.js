6611440f7d3973c3c88e00bffcba73a8697cf598
Fiona Weaver <fiona.l.weaver@gmail.com>
2021-03-02 15:15:51 -0700

    commit tree e042ad08239baceea776c3512bfb11ea52ba1e21

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let num3 = 8
+let num4 = 2
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 / num4
+
+const max = Math.max(2, 5, 6, 6, 20)
